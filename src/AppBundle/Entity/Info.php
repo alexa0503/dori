382c2f04003411652a9420f8275b2581a4bfe190
alexa0503 <lori.w@live.cn>
2015-10-07 22:13:15 +0800

    数据

--- src/AppBundle/Entity/Info.php
+++ src/AppBundle/Entity/Info.php
@@ -20,6 +20,18 @@ class Info
      */
     protected $username;
 
+
+    /**
+     * @ORM\Column(name="email",type="string", length=120)
+     */
+    protected $email;
+
+
+    /**
+     * @ORM\Column(name="company",type="string", length=120)
+     */
+    protected $company;
+
     /**
      * @ORM\Column(name="address",type="string", length=120)
      */
@@ -30,6 +42,7 @@ class Info
      * @ORM\Column(name="mobile",type="string", length=200)
      */
     protected $mobile;
+
     /**
      * @ORM\Column(name="create_time",  type="datetime")
      */
@@ -39,129 +52,4 @@ class Info
      * @ORM\Column(name="create_ip", type="string", length=60)
      */
     private $createIp;
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set address
-     *
-     * @param string $address
-     * @return Info
-     */
-    public function setAddress($address)
-    {
-        $this->address = $address;
-
-        return $this;
-    }
-
-    /**
-     * Get address
-     *
-     * @return string 
-     */
-    public function getAddress()
-    {
-        return $this->address;
-    }
-
-    /**
-     * Set mobile
-     *
-     * @param string $mobile
-     * @return Info
-     */
-    public function setMobile($mobile)
-    {
-        $this->mobile = $mobile;
-
-        return $this;
-    }
-
-    /**
-     * Get mobile
-     *
-     * @return string 
-     */
-    public function getMobile()
-    {
-        return $this->mobile;
-    }
-
-    /**
-     * Set createTime
-     *
-     * @param \DateTime $createTime
-     * @return Info
-     */
-    public function setCreateTime($createTime)
-    {
-        $this->createTime = $createTime;
-
-        return $this;
-    }
-
-    /**
-     * Get createTime
-     *
-     * @return \DateTime 
-     */
-    public function getCreateTime()
-    {
-        return $this->createTime;
-    }
-
-    /**
-     * Set createIp
-     *
-     * @param string $createIp
-     * @return Info
-     */
-    public function setCreateIp($createIp)
-    {
-        $this->createIp = $createIp;
-
-        return $this;
-    }
-
-    /**
-     * Get createIp
-     *
-     * @return string 
-     */
-    public function getCreateIp()
-    {
-        return $this->createIp;
-    }
-
-    /**
-     * Set username
-     *
-     * @param string $username
-     * @return Info
-     */
-    public function setUsername($username)
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    /**
-     * Get username
-     *
-     * @return string 
-     */
-    public function getUsername()
-    {
-        return $this->username;
-    }
 }
